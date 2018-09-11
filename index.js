bac0fcdbc53ee280d9e8d376c2bb3c13c06c1d5d
Lê Hữu Việt Anh <anhhvle@kms-technology.com>
2018-09-11 16:00:05 +0700

    second

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Put all API endpoints under '/api'
 app.get('/api/example', (req, res) => {
@@ -18,7 +23,7 @@ app.get('*', (req, res) => {
   res.sendStatus(404);
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5050;
 app.listen(port);
 
 console.log(`Server listening on ${port}`);
