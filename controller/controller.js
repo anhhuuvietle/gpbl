516e04a52a1bfe9495133828ca551a4433521ee4
Lê Hữu Việt Anh <anhhvle@kms-technology.com>
2018-09-14 06:14:33 +0700

    Done first time

--- controller/controller.js
+++ controller/controller.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 const Accident = require('../models/Accident');
 const geolib = require('geolib');
 
+const isOvertime = ( time0, time1) => {
+    const sub = time1.getMiliseconds() - time0.getMiliseconds();
+    if (sub >= 30*60*1000) return true;
+    return false;
+}
 module.exports = {
     handleInfo: async (req, res) => {
         // Add object to schema when user send info, check if it is too near
@@ -40,9 +45,50 @@ module.exports = {
     },
     handleLocation: async (req, res) => {
         // When user only send location, get data from db, consider what is out of date
-        // Check the distance and send back an array
-        const accidents = await Accident.find({status: true});
-        const accident = req.body;
+        let accidents = await Accident.find({status: true});
+        const { loc0, loc1 } = req.body;
+        const today = new Date();
+        let accs = [];
+        accidents.map(acc => {
+            // disable all accidents overtime
+            if (isOvertime(acc.time, today)) {
+                acc.status = false;
+                try {
+                    await Accident.findByIdAndUpdate(acc._id, { $set: { status: false }});
+                }
+                catch(e) {
+                    console.log(e);
+                }
+            }
+            else {
+                // Check the distance and send back an array
+                const dis0 = geolib.getDistance(
+                    {
+                        latitude: acc.lat,
+                        longitude: acc.long
+                    },
+                    {
+                        latitude: loc0.lat,
+                        longitude: loc0.long
+                    }
+                );
+                const dis1 = geolib.getDistance(
+                    {
+                        latitude: acc.lat,
+                        longitude: acc.long
+                    },
+                    {
+                        latitude: loc1.lat,
+                        longitude: loc1.long
+                    }
+                );
+                if (dis1 < dis0 && dis1 <= 1000 && dis1 > acc.rad) {
+                    accs = [...accs, acc];
+                }
 
+            }
+            return acc;
+        });
+        res.json(accs);
     }
 }
